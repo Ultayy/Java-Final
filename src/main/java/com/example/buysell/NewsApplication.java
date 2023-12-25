a541660edb2a64bfd56090f86bff7f03930881a7
Жулдыз Тынышбай <ultai.04@mail.ru>
2023-12-25 11:45:32 +0600

    first commit

---
+++ src/main/java/com/example/buysell/NewsApplication.java
@@ -0,0 +1,13 @@
+package com.example.buysell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication
+@EnableTransactionManagement
+public class NewsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NewsApplication.class, args);
+	}
+}
